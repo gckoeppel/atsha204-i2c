bee7b3e1cc9520c5dbc4f11f5ec81b9cdb46f945
Josh Datko <jbd@cryptotronix.com>
2014-10-03 22:19:48 +0000

    Adds simple test file

---
+++ test/test.c
@@ -0,0 +1,41 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+
+
+    int file;
+    char *filename = "/dev/atsha0";
+    static char buf[] = {0x03, 0x07, 0x1B, 0x01, 0x00, 0x00};
+    static char recv_buf[32];
+
+
+    if ((file = open(filename, O_RDWR)) < 0) {
+        /* ERROR HANDLING: you can check errno to see what went wrong */
+        perror("Failed to open the i2c bus");
+        exit(1);
+    }
+
+    if(sizeof(buf) != write(file,buf,sizeof(buf))){
+        perror("Write failed\n");
+        exit(1);
+    }
+
+    if (sizeof(recv_buf) != read(file,buf,sizeof(recv_buf))){
+        perror("Read failed\n");
+        exit(1);
+    }
+
+    close(file);
+
+    return 0;
+}
