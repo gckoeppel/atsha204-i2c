a6948a2fd20cd04ff8bff5948fe24591c6d176c6
Josh Datko <jbd@cryptotronix.com>
2014-10-29 14:39:17 +0000

    defaults to atsha204

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 sudo make modules_install
 sudo rmmod atsha204-i2c
 sudo insmod atsha204-i2c.ko
-echo atsha204-i2c 0x60 | sudo tee /sys/class/i2c-adapter/i2c-1/new_device
+#0x60 = ATECC108, 0x64 = ATSHA204
+#echo atsha204-i2c 0x60 | sudo tee /sys/class/i2c-adapter/i2c-1/new_device
+echo atsha204-i2c 0x64 | sudo tee /sys/class/i2c-adapter/i2c-1/new_device
 sudo chgrp i2c /dev/atsha0
 sudo chmod 664 /dev/atsha0
 test/test
